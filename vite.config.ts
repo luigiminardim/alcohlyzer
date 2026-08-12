66ff848b0e478c44928d66e84c4aaa41275de963
Luigi Minardi <luigiminardim.gmail.com>
2026-08-12 20:51:04 -0300

    feat: update application icons and PWA configuration for better branding

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: 'autoUpdate',
-      includeAssets: ['favicon.ico', 'apple-touch-icon.png'],
+      includeAssets: ['favicon.png', 'apple-touch-icon.png'],
       manifest: {
         name: 'Alcohlyzer',
         short_name: 'Alcohlyzer',
